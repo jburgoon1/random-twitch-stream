8f363f37160c39664ba1124561e5f1e80971e99c
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-27 15:44:30 -0600

    finished

---
+++ twitch-app/src/config.js
@@ -0,0 +1,5 @@
+const appKey = "g3cpadpxnnd9gxed8srkr30ulotv6b"
+const secret = "mvpagfy8nztsz26jwxqnrsf4zihmnp"
+const accessToken = "8r58425i828m45gn2eikqpsqe4of5c"
+
+export {appKey, secret, accessToken};
