56cfeac3e88720db2bf52b2f736c799bd1541a43
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-27 15:27:03 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = +process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 const userRoutes = require('./routes/user')
 const {authenticateJWT} = require('./middleware/auth')
 
-
+app.use(express.static(path.resolve(__dirname, '../twitch-app/build')));
 app.use(cors())
 app.use(express.json())
 app.use(authenticateJWT)
 app.use('/user', userRoutes)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../twitch-app/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
