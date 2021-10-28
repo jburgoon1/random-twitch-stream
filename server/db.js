8a0de7cb65c85d563de98c6275460a80f56f0760
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-28 13:22:24 -0600

    finished

--- server/db.js
+++ server/db.js
@@ -2,11 +2,9 @@ const { Client } = require("pg");
 
 let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///stream_test";
-} else {
+
   DB_URI = process.env.DATABASE_URL||"postgresql:///stream";
-}
+
 
 let db = new Client({
   connectionString: DB_URI
