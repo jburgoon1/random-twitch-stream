c4252f531157f56827ac9a8e5fd73b1dd46327b2
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-28 13:33:53 -0600

    finished

--- twitch-app/src/video.js
+++ twitch-app/src/video.js
@@ -12,12 +12,12 @@ export const Video = ({channel, id}) => {
         <div className="stream">
         <iframe
         title="twitch-stream"
-    src={`https://player.twitch.tv/?channel=${channel}&parent=https://randomtwitchstream.herokuapp.com/random&muted=false`}
+    src={`https://player.twitch.tv/?channel=${channel}&parent=randomtwitchstream.herokuapp.com/random&muted=false`}
     height="500"
     width="600"
     allowFullScreen={true}>
 </iframe>
-<iframe src={`https://www.twitch.tv/embed/${channel}/chat?parent=https://randomtwitchstream.herokuapp.com/random`}
+<iframe src={`https://www.twitch.tv/embed/${channel}/chat?parent=randomtwitchstream.herokuapp.com/random`}
         height="500"
         width="250">
 </iframe>
