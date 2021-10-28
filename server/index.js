b4851810067fd75903043b7e544704fd7e9c6988
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-28 12:00:46 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ app.use(express.static(path.resolve(__dirname, '../twitch-app/build')));
 app.use(cors())
 app.use(express.json())
 app.use(authenticateJWT)
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../twitch-app/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../twitch-app/build', 'index.html'));
+// });
 app.use('/user', userRoutes)
 
 
