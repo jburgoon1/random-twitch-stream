971cd0d642dce7c4307d426a2efad28880a9ee6f
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-10-31 22:36:38 -0600

    finished

--- twitch-app/src/video.js
+++ twitch-app/src/video.js
@@ -3,13 +3,18 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useEffect } from 'react'
 import ReactTwitchEmbedVideo from "react-twitch-embed-video"
+import {Container, Row, Col} from react-bootstrap;
 import "./video.css"
 export const Video = ({channel, id}) => {
     
    
 
     return (
-        <div className="stream">
+        <Container>
+            <Row>
+                
+        <div className="stream ">
+            <Col sm={12} lg={6}>
         <iframe
         title="twitch-stream"
     src={`https://player.twitch.tv/?channel=${channel}&parent=randomtwitchstream.herokuapp.com&muted=false`}
@@ -17,11 +22,16 @@ export const Video = ({channel, id}) => {
     width="600"
     allowFullScreen={true}>
 </iframe>
+</Col>
+<Col sm={12} lg={6}>
 <iframe src={`https://www.twitch.tv/embed/${channel}/chat?parent=randomtwitchstream.herokuapp.com`}
         height="500"
-        width="250">
+        width="400">
 </iframe>
+</Col>
 </div>
+</Row>
+</Container>
     )
 }
 
